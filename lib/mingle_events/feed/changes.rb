9e04d1c0fc1839d8c6c49c0c88e909b44f952a1e
David Rice <david.rice@gmail.com>
2011-09-01 16:47:47 -0700

    cannot be bothered to have custom parsers for each change if i am just building hashes; until (if ever) i introduce a rich model for changes (maybe never) this will do

--- lib/mingle_events/feed/changes.rb
+++ lib/mingle_events/feed/changes.rb
@@ -1,8 +1,3 @@
-require File.expand_path(File.join(File.dirname(__FILE__), 'change_builders', 'card_type_change')) 
-require File.expand_path(File.join(File.dirname(__FILE__), 'change_builders', 'no_detail_change')) 
-require File.expand_path(File.join(File.dirname(__FILE__), 'change_builders', 'name_change')) 
-require File.expand_path(File.join(File.dirname(__FILE__), 'change_builders', 'property_change')) 
-
 module MingleEvents  
   module Feed
     
@@ -11,12 +6,6 @@ module MingleEvents
       
       include Enumerable
       
-      CUSTOM_BUILDERS = {
-        Category::CARD_TYPE_CHANGE => ChangeBuilders::CardTypeChange.new,
-        Category::NAME_CHANGE => ChangeBuilders::NameChange.new,
-        Category::PROPERTY_CHANGE => ChangeBuilders::PropertyChange.new
-      }
-            
       def initialize(changes_element)
         @changes_element = changes_element
       end
@@ -31,11 +20,56 @@ module MingleEvents
         changes = []
         @changes_element.search("change").map do |change_element|
           category = Category.for_mingle_term(change_element["type"])
-          builder = CUSTOM_BUILDERS[category] || ChangeBuilders::NoDetailChange.new(category)
-          changes <<  builder.build(change_element)
+          changes <<  Change.new(category).build(change_element)
         end
         changes
       end
+      
+      class Change
+        
+        def initialize(category)
+          @category = category
+        end
+        
+        def build(element)          
+          element_to_hash(element)
+                                    
+          raw_hash_from_xml = element_to_hash(element)
+          
+          raw_hash_from_xml[:change].merge({
+            :category => @category, 
+            :type => @category
+          })
+        end
+        
+        private
+    
+        def element_to_hash(element, hash = {}) 
+          hash_for_element = (hash[element.name.to_sym] ||= {})
+                   
+          element.attribute_nodes.each do |a|
+            hash_for_element[a.name.to_sym] = a.value
+          end
+                   
+          element.children.each do |child|
+            next unless child.is_a?(Nokogiri::XML::Element)
+            
+            if child.children.count{|c| c.is_a?(Nokogiri::XML::Element)} > 0
+              element_to_hash(child, hash_for_element)
+            else
+              hash_for_element[child.name.to_sym] = if child["nil"] && child["nil"] == "true"
+                nil
+              else
+                child.inner_text
+              end
+            end
+          end 
+          
+          hash         
+        end
+                        
+      end      
+      
 
     end    
   end
--- test/mingle_events/feed/changes_test.rb
+++ test/mingle_events/feed/changes_test.rb
@@ -36,7 +36,7 @@ module MingleEvents
         
         # check that a change's detail is built
         card_type_change = entry.changes.find{|c| c[:category] == Category::CARD_TYPE_CHANGE}
-        assert_equal('Defect', card_type_change[:new_value][:name])
+        assert_equal('Defect', card_type_change[:new_value][:card_type][:name])
       end
             
       def test_parse_name_change_from_nil
@@ -68,13 +68,13 @@ module MingleEvents
                 <change type="name-change">
                   <old_value>Work with email</old_value>
                   <new_value>Basic email integration</new_value>
-                </change
+                </change>
               </changes>
             </content>
           </entry>}
         entry = Entry.from_snippet(element_xml_text)
         
-        change = entry.changes.first
+        change = entry.changes.first        
         assert_equal(Category::NAME_CHANGE, change[:type])
         assert_equal(Category::NAME_CHANGE, change[:category])
         assert_equal("Work with email", change[:old_value])
@@ -115,11 +115,12 @@ module MingleEvents
         entry = Entry.from_snippet(element_xml_text)
         
         change = entry.changes.first
+        
         assert_equal(Category::CARD_TYPE_CHANGE, change[:type])
         assert_equal(Category::CARD_TYPE_CHANGE, change[:category])
         assert_nil change[:old_value]
-        assert_equal("Defect", change[:new_value][:name])
-        assert_equal("https://mingle.example.com/api/v2/projects/atlas/card_types/27.xml", change[:new_value][:url])
+        assert_equal("Defect", change[:new_value][:card_type][:name])
+        assert_equal("https://mingle.example.com/api/v2/projects/atlas/card_types/27.xml", change[:new_value][:card_type][:url])
       end
       
       def test_parse_card_type_change
@@ -130,7 +131,7 @@ module MingleEvents
                 <change type="card-type-change">
                   <old_value>
                     <card_type url="https://mingle.example.com/api/v2/projects/atlas/card_types/30.xml">
-                      <name>Story</name>
+                      <name>Story</name
                     </card_type>
                   </old_value>
                   <new_value>
@@ -147,10 +148,10 @@ module MingleEvents
         change = entry.changes.first
         assert_equal(Category::CARD_TYPE_CHANGE, change[:type])
         assert_equal(Category::CARD_TYPE_CHANGE, change[:category])
-        assert_equal("Story", change[:old_value][:name])
-        assert_equal("https://mingle.example.com/api/v2/projects/atlas/card_types/30.xml", change[:old_value][:url])
-        assert_equal("Defect", change[:new_value][:name])
-        assert_equal("https://mingle.example.com/api/v2/projects/atlas/card_types/27.xml", change[:new_value][:url])
+        assert_equal("Story", change[:old_value][:card_type][:name])
+        assert_equal("https://mingle.example.com/api/v2/projects/atlas/card_types/30.xml", change[:old_value][:card_type][:url])
+        assert_equal("Defect", change[:new_value][:card_type][:name])
+        assert_equal("https://mingle.example.com/api/v2/projects/atlas/card_types/27.xml", change[:new_value][:card_type][:url])
       end
       
       def test_parse_card_type_change_to_deleted_type
@@ -178,11 +179,10 @@ module MingleEvents
         change = entry.changes.first
         assert_equal(Category::CARD_TYPE_CHANGE, change[:type])
         assert_equal(Category::CARD_TYPE_CHANGE, change[:category])
-        assert_equal("Story", change[:old_value][:name])
-        assert_equal("https://mingle.example.com/api/v2/projects/atlas/card_types/30.xml", change[:old_value][:url])
-        assert_equal("Card", change[:new_value][:name])
-        assert change[:new_value][:deleted?]
-        assert_equal(nil, change[:new_value][:url])
+        assert_equal("Story", change[:old_value][:card_type][:name])
+        assert_equal("https://mingle.example.com/api/v2/projects/atlas/card_types/30.xml", change[:old_value][:card_type][:url])
+        assert_equal("Card", change[:new_value][:deleted_card_type][:name])
+        assert_equal(nil, change[:new_value][:deleted_card_type][:url])
       end     
       
       def test_parse_card_property_change
